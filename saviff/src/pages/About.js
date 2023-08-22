f8e6fac3a608942619aa5b133595d2a2dcd1d40e
Abhishek <abhishek84sia1726@gmail.com>
2023-08-22 16:26:26 +0530

    initial commit

--- saviff/src/pages/About.js
+++ saviff/src/pages/About.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from "react";
+import Layout from "./../components/Layout/Layout";
 
 const About = () => {
   return (
-    <Layout>
-        <h1>
-        About
-        </h1>
-    </Layout>
-  )
-}
+    <Layout title={"Saviff-Aboutus"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+          **About Us**<br />
 
-export default About
+Welcome to <b>SAVIFF</b>, your premier destination for renting exquisite partywear and a diverse range of clothing for men and women. Our curated collection offers a variety of styles to suit every occasion. Choose us for quality, convenience, and sustainability, as we redefine fashion by offering premium rentals at your fingertips. Join us in celebrating self-expression and making each moment memorable without the burden of ownership. Thank you for choosing <b>SAVIFF</b> to be a part of your unique journey in style.
+
+
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
