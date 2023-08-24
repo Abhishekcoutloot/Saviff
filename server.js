38b5d74ff8a4c25839a8e819ad32c6321863d8c9
Abhishek <abhishek84sia1726@gmail.com>
2023-08-24 17:18:29 +0530

    deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js"
 import cors from "cors"
+import path from "path ";
 
 //config env
 dotenv.config();
@@ -20,12 +21,15 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname,"./saviff/build")))
 
 
 
 //routes
 
-app.use("/api/v1/auth", authRoutes);
+app.use("*", function(req, res){
+    res.send(path.join(__dirname,"./saviff/build/index.html"))
+});
 
 
 
